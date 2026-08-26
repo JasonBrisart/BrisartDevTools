6b6e9f9c5365ae8fbb21e3d9b999e9772123e836
Jason Brisart <jason@brisartresearcharchive.com>
2026-08-26 02:47:49 -0400

    Bump ProjectContextHelper version
    Update the application version from 3.1.2 to 3.1.2.1 in the shared constants so the release metadata matches the patched build.

--- ProjectContextHelper/core/constants.py
+++ ProjectContextHelper/core/constants.py
@@ -1,7 +1,7 @@
 from core.models import ScanSettings
 
 APP_NAME = "Project Context Helper"
-APP_VERSION = "3.1.2"
+APP_VERSION = "3.1.2.1"
 AUTHOR = "Jason Brisart"
 REPOSITORY_NAME = "BrisartDevTools"
 REPOSITORY_URL = "https://github.com/JasonBrisart/BrisartDevTools"
